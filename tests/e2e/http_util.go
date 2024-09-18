fb29f09137db80bd9b27bd26290f2f9060097679
Thomas Bruyelle <thomas.bruyelle@tendermint.com>
2024-09-18 19:09:50 +0200

    test(e2e): httpGet to read response status and retry if Service Unavailable

--- tests/e2e/http_util.go
+++ tests/e2e/http_util.go
@@ -5,14 +5,30 @@ import (
 	"fmt"
 	"io"
 	"net/http"
+	"time"
 )
 
 func httpGet(endpoint string) ([]byte, error) {
+	return httpGet_(endpoint, 0)
+}
+
+const maxAttempt = 5
+
+func httpGet_(endpoint string, attempt int) ([]byte, error) {
 	resp, err := http.Get(endpoint) //nolint:gosec // this is only used during tests
 	if err != nil {
 		return nil, fmt.Errorf("failed to execute HTTP request: %w", err)
 	}
 	defer resp.Body.Close()
+
+	if resp.StatusCode == http.StatusServiceUnavailable && attempt < maxAttempt {
+		// node not avail, wait and retry
+		time.Sleep(time.Second)
+		return httpGet_(endpoint, attempt+1)
+	}
+	if resp.StatusCode != http.StatusOK {
+		return nil, fmt.Errorf("server responsed status %s", resp.Status)
+	}
 
 	body, err := io.ReadAll(resp.Body)
 	if err != nil {
