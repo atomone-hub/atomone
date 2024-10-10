bdae7284beed327d265a22a2d51c9a1e6cc9d270
Thomas Bruyelle <thomas.bruyelle@tendermint.com>
2024-10-10 14:44:54 +0200

    photon cli conversion-rate

--- x/photon/client/cli/query.go
+++ x/photon/client/cli/query.go
@@ -6,6 +6,7 @@ import (
 	"github.com/spf13/cobra"
 
 	"github.com/cosmos/cosmos-sdk/client"
+	"github.com/cosmos/cosmos-sdk/client/flags"
 
 	"github.com/atomone-hub/atomone/x/photon/types"
 )
@@ -21,6 +22,53 @@ func GetQueryCmd(queryRoute string) *cobra.Command {
 		RunE:                       client.ValidateCmd,
 	}
 
-	cmd.AddCommand(CmdQueryParams())
+	cmd.AddCommand(
+		GetQueryParamsCmd(),
+		GetQueryConversionRateCmd(),
+	)
+	return cmd
+}
+
+func GetQueryParamsCmd() *cobra.Command {
+	cmd := &cobra.Command{
+		Use:   "params",
+		Short: "shows the parameters of the module",
+		Args:  cobra.NoArgs,
+		RunE: func(cmd *cobra.Command, args []string) error {
+			clientCtx, err := client.GetClientQueryContext(cmd)
+			if err != nil {
+				return err
+			}
+			queryClient := types.NewQueryClient(clientCtx)
+			res, err := queryClient.Params(cmd.Context(), &types.QueryParamsRequest{})
+			if err != nil {
+				return err
+			}
+			return clientCtx.PrintProto(res)
+		},
+	}
+	flags.AddQueryFlagsToCmd(cmd)
+	return cmd
+}
+
+func GetQueryConversionRateCmd() *cobra.Command {
+	cmd := &cobra.Command{
+		Use:   "conversion-rate",
+		Short: "shows the atone to photon conversion rate",
+		Args:  cobra.NoArgs,
+		RunE: func(cmd *cobra.Command, args []string) error {
+			clientCtx, err := client.GetClientQueryContext(cmd)
+			if err != nil {
+				return err
+			}
+			queryClient := types.NewQueryClient(clientCtx)
+			res, err := queryClient.ConversionRate(cmd.Context(), &types.QueryConversionRateRequest{})
+			if err != nil {
+				return err
+			}
+			return clientCtx.PrintProto(res)
+		},
+	}
+	flags.AddQueryFlagsToCmd(cmd)
 	return cmd
 }
