ce3dd6a9a56c67f8e53ef15c1d0e85563e002a9e
Thomas Bruyelle <thomas.bruyelle@tendermint.com>
2024-10-10 08:50:25 +0200

    apply conversion

--- x/photon/keeper/msg_server.go
+++ x/photon/keeper/msg_server.go
@@ -20,6 +20,8 @@ func NewMsgServerImpl(keeper Keeper) types.MsgServer {
 
 var _ types.MsgServer = msgServer{}
 
+const photonMaxSupply = 1_000_000_000
+
 func (k msgServer) Burn(goCtx context.Context, msg *types.MsgBurn) (*types.MsgBurnResponse, error) {
 	ctx := sdk.UnwrapSDKContext(goCtx)
 	params := k.GetParams(ctx)
@@ -27,26 +29,51 @@ func (k msgServer) Burn(goCtx context.Context, msg *types.MsgBurn) (*types.MsgBu
 		return nil, types.ErrMintDisabled
 	}
 
-	// Ensure burned amount denom is bond denom (atone)
+	// Ensure burned amount denom is bond denom (uatone)
+	// TODO ensure it is uatone
 	bondDenom := k.stakingKeeper.BondDenom(ctx)
 	if msg.Amount.Denom != bondDenom {
-		return nil, types.ErrMintInvalidDenom
+		return nil, types.ErrBurnInvalidDenom
 	}
-	// Send amount to photon module
+	// Compute photons to mint
+	var (
+		atomToBurn            = msg.Amount
+		atoneSupply           = k.bankKeeper.GetSupply(ctx, bondDenom)
+		photonSupply          = k.bankKeeper.GetSupply(ctx, "uphoton")
+		remainMintablePhotons = sdk.NewInt(photonMaxSupply).Sub(photonSupply.Amount)
+		photonToMint          = atomToBurn.Amount.Mul(
+			remainMintablePhotons.Quo(atoneSupply.Amount),
+		)
+	)
+
+	if photonToMint.IsZero() {
+		return nil, types.ErrNoMintablePhotons
+	}
+	// TODO check if photonToMint + remainMintablePhotons > photonMaxSupply ?
 	to, err := sdk.AccAddressFromBech32(msg.ToAddress)
 	if err != nil {
 		return nil, err
 	}
-	coins := sdk.NewCoins(msg.Amount)
-	if err := k.bankKeeper.SendCoinsFromAccountToModule(ctx, to, types.ModuleName, coins); err != nil {
-		return nil, err
-	}
-	// Burn amount
-	if err := k.bankKeeper.BurnCoins(ctx, types.ModuleName, coins); err != nil {
-		return nil, err
-	}
-	// Compute photons to mint
-	// TODO
 
-	return &types.MsgBurnResponse{}, nil
+	// Send atone to photon module for burn
+	coinsToBurn := sdk.NewCoins(atomToBurn)
+	if err := k.bankKeeper.SendCoinsFromAccountToModule(ctx, to, types.ModuleName, coinsToBurn); err != nil {
+		return nil, err
+	}
+	// Burn atone
+	if err := k.bankKeeper.BurnCoins(ctx, types.ModuleName, coinsToBurn); err != nil {
+		return nil, err
+	}
+
+	// Mint photons
+	coinsToMint := sdk.NewCoins(sdk.NewCoin("uphoton", photonToMint))
+	if err := k.bankKeeper.MintCoins(ctx, types.ModuleName, coinsToMint); err != nil {
+		return nil, err
+	}
+	// Send minted photon to account
+	if err := k.bankKeeper.SendCoinsFromModuleToAccount(ctx, types.ModuleName, to, coinsToMint); err != nil {
+		return nil, err
+	}
+
+	return &types.MsgBurnResponse{Minted: coinsToMint[0]}, nil
 }
