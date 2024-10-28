d142c79196e3f5523a8119d2f15f9e0cb418f863
Thomas Bruyelle <thomas.bruyelle@tendermint.com>
2024-10-28 11:46:05 +0100

    test: add TestMsgServerUpdateParams

--- x/photon/genesis_test.go
+++ x/photon/genesis_test.go
@@ -3,23 +3,22 @@ package photon_test
 import (
 	"testing"
 
-	keepertest "github.com/atomone-hub/atomone/testutil/keeper"
-	"github.com/atomone-hub/atomone/testutil/nullify"
-	"github.com/atomone-hub/atomone/x/photon"
-	"github.com/atomone-hub/atomone/x/photon/types"
 	"github.com/stretchr/testify/require"
+
+	"github.com/atomone-hub/atomone/x/photon"
+	"github.com/atomone-hub/atomone/x/photon/testutil"
+	"github.com/atomone-hub/atomone/x/photon/types"
 )
 
 func TestGenesis(t *testing.T) {
 	genesisState := types.GenesisState{
 		Params: types.DefaultParams(),
 	}
+	k, _, ctx := testutil.SetupPhotonKeeper(t)
 
-	k, ctx := keepertest.PhotonKeeper(t)
 	photon.InitGenesis(ctx, *k, genesisState)
 	got := photon.ExportGenesis(ctx, *k)
-	require.NotNil(t, got)
 
-	nullify.Fill(&genesisState)
-	nullify.Fill(got)
+	require.NotNil(t, got)
+	require.Equal(t, genesisState, *got)
 }
