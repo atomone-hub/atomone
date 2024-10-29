0bd5debda3905666d6255d8775441888b418e723
Thomas Bruyelle <thomas.bruyelle@tendermint.com>
2024-10-29 18:24:47 +0100

    test: photon simulation

---
+++ x/photon/simulation/genesis.go
@@ -0,0 +1,33 @@
+package simulation
+
+import (
+	"math/rand"
+
+	"github.com/atomone-hub/atomone/x/photon/types"
+
+	"github.com/cosmos/cosmos-sdk/types/module"
+)
+
+const (
+	MintDisabled = "mint_disabled"
+)
+
+// GenMintDisabled returns a randomized MintDisabled param.
+func GenMintDisabled(r *rand.Rand) bool {
+	return r.Int63n(101) <= 15 // 15% chance of mint being disabled
+}
+
+// RandomizedGenState generates a random GenesisState for gov
+func RandomizedGenState(simState *module.SimulationState) {
+	var mintDisabled bool
+	simState.AppParams.GetOrGenerate(
+		simState.Cdc, MintDisabled, &mintDisabled, simState.Rand,
+		func(r *rand.Rand) { mintDisabled = GenMintDisabled(r) },
+	)
+
+	photonGenesis := types.NewGenesisState(
+		types.NewParams(mintDisabled),
+	)
+
+	simState.GenState[types.ModuleName] = simState.Cdc.MustMarshalJSON(photonGenesis)
+}
