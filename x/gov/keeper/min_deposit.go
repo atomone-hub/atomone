2a0f37613d797465bd261817328aabb7dae3d9fe
Giuseppe Natale <12249307+giunatale@users.noreply.github.com>
2024-12-10 17:08:03 +0100

    fix sign of percChange

--- x/gov/keeper/min_deposit.go
+++ x/gov/keeper/min_deposit.go
@@ -105,7 +105,12 @@ func (keeper Keeper) GetMinDeposit(ctx sdk.Context) sdk.Coins {
 			b = numActiveProposals.Sub(targetActiveProposals).ToLegacyDec()
 		}
 		c := a.Mul(b)
-		percChange = percChange.Add(c.Power(uint64(ticksPassed)))
+		cPow := c.Power(uint64(ticksPassed))
+		if c.IsNegative() && !cPow.IsNegative() {
+			// ensure that the percentage change is negative if c is negative
+			cPow = cPow.Neg()
+		}
+		percChange = percChange.Add(cPow)
 	}
 
 	minDeposit := sdk.Coins{}
