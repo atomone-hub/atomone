84439d705e9d27011c41809ac10aa8528c658d69
Thomas Bruyelle <thomas.bruyelle@tendermint.com>
2025-03-03 14:23:42 +0100

    chore(x/gov): add module migration to initialize new params (#89)
    I had to change the CI `action/cache` version because the version was
    deprecated.

--- x/gov/keeper/migrations.go
+++ x/gov/keeper/migrations.go
@@ -1,7 +1,10 @@
 package keeper
 
 import (
+	sdk "github.com/cosmos/cosmos-sdk/types"
+
 	"github.com/atomone-hub/atomone/x/gov/exported"
+	v5 "github.com/atomone-hub/atomone/x/gov/migrations/v5"
 )
 
 // Migrator is a struct for handling in-place store migrations.
@@ -16,4 +19,9 @@ func NewMigrator(keeper *Keeper, legacySubspace exported.ParamSubspace) Migrator
 		keeper:         keeper,
 		legacySubspace: legacySubspace,
 	}
+}
+
+// Migrate4to5 migrates the store from version 4 to 5.
+func (m Migrator) Migrate4to5(ctx sdk.Context) error {
+	return v5.MigrateStore(ctx, m.keeper.storeKey, m.keeper.cdc)
 }
