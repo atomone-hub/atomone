903de6f3d65fde80d0540c477642e74e0b3f1f6b
Giuseppe Natale <12249307+giunatale@users.noreply.github.com>
2025-03-12 19:02:42 +0100

    add comment

--- app/keepers/keys.go
+++ app/keepers/keys.go
@@ -33,7 +33,6 @@ func (appKeepers *AppKeepers) GenerateKeys() {
 		authtypes.StoreKey,
 		banktypes.StoreKey,
 		stakingtypes.StoreKey,
-		crisistypes.StoreKey,
 		minttypes.StoreKey,
 		distrtypes.StoreKey,
 		slashingtypes.StoreKey,
@@ -49,6 +48,11 @@ func (appKeepers *AppKeepers) GenerateKeys() {
 		authzkeeper.StoreKey,
 		consensusparamtypes.StoreKey,
 		photontypes.StoreKey,
+		// TODO: to be removed in a future release, since x/crisis was
+		// deprecated. The key had to be left here to facilitate
+		// deletion of the module's state from the store during the 
+		// software upgrade.
+		crisistypes.StoreKey, 
 	)
 
 	// Define transient store keys
