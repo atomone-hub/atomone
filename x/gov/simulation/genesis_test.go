51751dc983f2bc50ca1d9e4f0c10487c0fff7a53
Giuseppe Natale <12249307+giunatale@users.noreply.github.com>
2025-03-18 14:18:02 +0100

    typos

--- x/gov/simulation/genesis_test.go
+++ x/gov/simulation/genesis_test.go
@@ -73,7 +73,7 @@ func TestRandomizedGenState(t *testing.T) {
 	require.Equal(t, "", govGenesis.Constitution)
 }
 
-// TestRandomizedGenState tests abnormal scenarios of applying RandomizedGenState.
+// TestRandomizedGenState1 tests abnormal scenarios of applying RandomizedGenState.
 func TestRandomizedGenState1(t *testing.T) {
 	interfaceRegistry := codectypes.NewInterfaceRegistry()
 	cdc := codec.NewProtoCodec(interfaceRegistry)
