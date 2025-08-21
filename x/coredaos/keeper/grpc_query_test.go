8e20b9b21eb80cc7dbbd1ca4b49a48b314f5e3d2
Giulio Stramondo <giuliostramondo@gmail.com>
2025-08-21 15:56:49 +0200

    test(coredaos): unit tests for the coredaos module (#199)
    This PR contains the unit test for the coredaos module #188 .
    
    The CI will fail at this time, but it is a feature.

---
+++ x/coredaos/keeper/grpc_query_test.go
@@ -0,0 +1,20 @@
+package keeper_test
+
+import (
+	"testing"
+
+	"github.com/atomone-hub/atomone/x/coredaos/testutil"
+	"github.com/atomone-hub/atomone/x/coredaos/types"
+	"github.com/stretchr/testify/require"
+)
+
+func TestParamsQuery(t *testing.T) {
+	k, _, ctx := testutil.SetupCoredaosKeeper(t)
+	params := types.DefaultParams()
+	k.SetParams(ctx, params)
+
+	resp, err := k.Params(ctx, &types.QueryParamsRequest{})
+
+	require.NoError(t, err)
+	require.Equal(t, &types.QueryParamsResponse{Params: params}, resp)
+}
