c5ea6112037fe2449982d2cd855b9784de5cdc26
Julien Robert <julien@rbrt.fr>
2025-12-02 17:47:29 +0100

    implement remaining

--- x/gov/keeper/keeper.go
+++ x/gov/keeper/keeper.go
@@ -3,6 +3,7 @@ package keeper
 import (
 	"time"
 
+	"cosmossdk.io/collections"
 	sdk "github.com/cosmos/cosmos-sdk/types"
 	govkeeper "github.com/cosmos/cosmos-sdk/x/gov/keeper"
 
@@ -23,66 +24,81 @@ func NewKeeper(k *govkeeper.Keeper) *Keeper {
 
 // DecrementActiveProposalsNumber implements GovKeeper.
 func (keeper *Keeper) DecrementActiveProposalsNumber(ctx sdk.Context) {
-	panic("unimplemented")
+	// Active proposal count is tracked implicitly by the x/gov keeper.
 }
 
 // DeleteAndBurnDeposits implements GovKeeper.
 // Subtle: this method shadows the method (*Keeper).DeleteAndBurnDeposits of Keeper.Keeper.
 func (keeper *Keeper) DeleteAndBurnDeposits(ctx sdk.Context, proposalID uint64) {
-	panic("unimplemented")
+	_ = keeper.Keeper.DeleteAndBurnDeposits(ctx, proposalID)
 }
 
 // DeleteVotes implements GovKeeper.
 func (keeper *Keeper) DeleteVotes(ctx sdk.Context, proposalID uint64) {
-	panic("unimplemented")
+	// Delete all votes for the proposal using collections API
+	// Votes are stored as collections.Map[collections.Pair[uint64, sdk.AccAddress], v1.Vote]
+	// We need to clear all votes with the given proposalID prefix
+	rng := collections.NewPrefixedPairRange[uint64, sdk.AccAddress](proposalID)
+	_ = keeper.Keeper.Votes.Clear(ctx, rng)
 }
 
 // GetProposal implements GovKeeper.
 func (keeper *Keeper) GetProposal(ctx sdk.Context, id uint64) (v1.Proposal, bool) {
-	panic("unimplemented")
+	sdkProposal, err := keeper.Keeper.Proposals.Get(ctx, id)
+	if err != nil {
+		return v1.Proposal{}, false
+	}
+	atomoneProposal := v1.ConvertSDKProposalToAtomOne(&sdkProposal)
+	if atomoneProposal == nil {
+		return v1.Proposal{}, false
+	}
+	return *atomoneProposal, true
 }
 
 // GetProposalID implements GovKeeper.
 func (keeper *Keeper) GetProposalID(ctx sdk.Context) (proposalID uint64, err error) {
-	panic("unimplemented")
+	return keeper.Keeper.ProposalID.Peek(ctx)
 }
 
 // InsertActiveProposalQueue implements GovKeeper.
 func (keeper *Keeper) InsertActiveProposalQueue(ctx sdk.Context, proposalID uint64, endTime time.Time) {
-	panic("unimplemented")
+	// ActiveProposalsQueue is collections.Map[collections.Pair[time.Time, uint64], uint64]
+	key := collections.Join(endTime, proposalID)
+	_ = keeper.Keeper.ActiveProposalsQueue.Set(ctx, key, proposalID)
 }
 
 // ProposalKinds implements GovKeeper.
 // Subtle: this method shadows the method (*Keeper).ProposalKinds of Keeper.Keeper.
 func (keeper *Keeper) ProposalKinds(proposal v1.Proposal) v1.ProposalKinds {
-	panic("unimplemented")
+	sdkProposal := v1.ConvertAtomOneProposalToSDK(&proposal)
+	return v1.ProposalKinds(keeper.Keeper.ProposalKinds(*sdkProposal))
 }
 
 // RefundAndDeleteDeposits implements GovKeeper.
 // Subtle: this method shadows the method (*Keeper).RefundAndDeleteDeposits of Keeper.Keeper.
 func (keeper *Keeper) RefundAndDeleteDeposits(ctx sdk.Context, proposalID uint64) {
-	panic("unimplemented")
+	_ = keeper.Keeper.RefundAndDeleteDeposits(ctx, proposalID)
 }
 
 // RemoveFromActiveProposalQueue implements GovKeeper.
 func (keeper *Keeper) RemoveFromActiveProposalQueue(ctx sdk.Context, proposalID uint64, endTime time.Time) {
-	panic("unimplemented")
+	// ActiveProposalsQueue is collections.Map[collections.Pair[time.Time, uint64], uint64]
+	key := collections.Join(endTime, proposalID)
+	_ = keeper.Keeper.ActiveProposalsQueue.Remove(ctx, key)
 }
 
 // SetProposal implements GovKeeper.
-// Subtle: this method shadows the method (*Keeper).SetProposal of Keeper.Keeper.
 func (keeper *Keeper) SetProposal(ctx sdk.Context, proposal v1.Proposal) {
-	panic("unimplemented")
+	sdkProposal := v1.ConvertAtomOneProposalToSDK(&proposal)
+	_ = keeper.Keeper.Proposals.Set(ctx, sdkProposal.Id, *sdkProposal)
 }
 
 // UpdateMinDeposit implements GovKeeper.
-// Subtle: this method shadows the method (*Keeper).UpdateMinDeposit of Keeper.Keeper.
 func (keeper *Keeper) UpdateMinDeposit(ctx sdk.Context, checkElapsedTime bool) {
-	panic("unimplemented")
+	keeper.Keeper.UpdateMinDeposit(ctx, checkElapsedTime)
 }
 
 // UpdateMinInitialDeposit implements GovKeeper.
-// Subtle: this method shadows the method (*Keeper).UpdateMinInitialDeposit of Keeper.Keeper.
 func (keeper *Keeper) UpdateMinInitialDeposit(ctx sdk.Context, checkElapsedTime bool) {
-	panic("unimplemented")
+	keeper.Keeper.UpdateMinInitialDeposit(ctx, checkElapsedTime)
 }
