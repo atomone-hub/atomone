aaf4b782032405dfe481f1816b371003b395b650
Julien Robert <julien@rbrt.fr>
2025-12-02 17:16:16 +0100

    expose proper state

---
+++ x/gov/keeper/expected_keepers.go
@@ -0,0 +1,47 @@
+package keeper
+
+import (
+	"time"
+
+	sdk "github.com/cosmos/cosmos-sdk/types"
+
+	v1 "github.com/atomone-hub/atomone/x/gov/types/v1"
+)
+
+var _ GovKeeper = (*Keeper)(nil)
+
+// GovKeeper defines the expected interface needed to interact with the
+// governance module.
+type GovKeeper interface {
+	// GetProposalID gets the highest proposal ID
+	GetProposalID(ctx sdk.Context) (proposalID uint64, err error)
+	// GetProposal gets a proposal from store by ProposalID.
+	GetProposal(ctx sdk.Context, id uint64) (v1.Proposal, bool)
+	// SetProposal sets a proposal in the gov store.
+	SetProposal(ctx sdk.Context, proposal v1.Proposal)
+	// ProposalKinds returns a v1.ProposalKinds useful to determine which kind
+	// of messages are included in a proposal.
+	ProposalKinds(proposal v1.Proposal) v1.ProposalKinds
+	// DeleteAndBurnDeposits deletes and burns all the deposits on a
+	// specific proposal.
+	DeleteAndBurnDeposits(ctx sdk.Context, proposalID uint64)
+	// RefundAndDeleteDeposits refunds and deletes all the deposits on a
+	// specific proposal.
+	RefundAndDeleteDeposits(ctx sdk.Context, proposalID uint64)
+	// InsertActiveProposalQueue inserts a proposalID into the active proposal
+	// queue at endTime
+	InsertActiveProposalQueue(ctx sdk.Context, proposalID uint64, endTime time.Time)
+	// RemoveFromActiveProposalQueue removes a proposalID from the Active
+	// Proposal Queue
+	RemoveFromActiveProposalQueue(ctx sdk.Context, proposalID uint64, endTime time.Time)
+	// DecrementActiveProposalsNumber decrements the number of active proposals
+	// by one
+	DecrementActiveProposalsNumber(ctx sdk.Context)
+	// UpdateMinInitialDeposit updates the min initial deposit required for
+	// proposal submission
+	UpdateMinInitialDeposit(ctx sdk.Context, checkElapsedTime bool)
+	// UpdateMinDeposit updates the minimum deposit required for a proposal
+	UpdateMinDeposit(ctx sdk.Context, checkElapsedTime bool)
+	// DeleteVotes deletes all votes from a proposal with given proposalID
+	DeleteVotes(ctx sdk.Context, proposalID uint64)
+}
--- x/gov/keeper/keeper.go
+++ x/gov/keeper/keeper.go
@@ -1,12 +1,12 @@
 package keeper
 
 import (
-	"cosmossdk.io/log"
+	"time"
 
 	sdk "github.com/cosmos/cosmos-sdk/types"
 	govkeeper "github.com/cosmos/cosmos-sdk/x/gov/keeper"
 
-	"github.com/atomone-hub/atomone/x/gov/types"
+	v1 "github.com/atomone-hub/atomone/x/gov/types/v1"
 )
 
 // Keeper defines the governance module Keeper
@@ -21,7 +21,68 @@ func NewKeeper(k *govkeeper.Keeper) *Keeper {
 	}
 }
 
-// Logger returns a module-specific logger.
-func (keeper Keeper) Logger(ctx sdk.Context) log.Logger {
-	return ctx.Logger().With("module", "x/"+types.ModuleName)
+// DecrementActiveProposalsNumber implements GovKeeper.
+func (keeper *Keeper) DecrementActiveProposalsNumber(ctx sdk.Context) {
+	panic("unimplemented")
+}
+
+// DeleteAndBurnDeposits implements GovKeeper.
+// Subtle: this method shadows the method (*Keeper).DeleteAndBurnDeposits of Keeper.Keeper.
+func (keeper *Keeper) DeleteAndBurnDeposits(ctx sdk.Context, proposalID uint64) {
+	panic("unimplemented")
+}
+
+// DeleteVotes implements GovKeeper.
+func (keeper *Keeper) DeleteVotes(ctx sdk.Context, proposalID uint64) {
+	panic("unimplemented")
+}
+
+// GetProposal implements GovKeeper.
+func (keeper *Keeper) GetProposal(ctx sdk.Context, id uint64) (v1.Proposal, bool) {
+	panic("unimplemented")
+}
+
+// GetProposalID implements GovKeeper.
+func (keeper *Keeper) GetProposalID(ctx sdk.Context) (proposalID uint64, err error) {
+	panic("unimplemented")
+}
+
+// InsertActiveProposalQueue implements GovKeeper.
+func (keeper *Keeper) InsertActiveProposalQueue(ctx sdk.Context, proposalID uint64, endTime time.Time) {
+	panic("unimplemented")
+}
+
+// ProposalKinds implements GovKeeper.
+// Subtle: this method shadows the method (*Keeper).ProposalKinds of Keeper.Keeper.
+func (keeper *Keeper) ProposalKinds(proposal v1.Proposal) v1.ProposalKinds {
+	panic("unimplemented")
+}
+
+// RefundAndDeleteDeposits implements GovKeeper.
+// Subtle: this method shadows the method (*Keeper).RefundAndDeleteDeposits of Keeper.Keeper.
+func (keeper *Keeper) RefundAndDeleteDeposits(ctx sdk.Context, proposalID uint64) {
+	panic("unimplemented")
+}
+
+// RemoveFromActiveProposalQueue implements GovKeeper.
+func (keeper *Keeper) RemoveFromActiveProposalQueue(ctx sdk.Context, proposalID uint64, endTime time.Time) {
+	panic("unimplemented")
+}
+
+// SetProposal implements GovKeeper.
+// Subtle: this method shadows the method (*Keeper).SetProposal of Keeper.Keeper.
+func (keeper *Keeper) SetProposal(ctx sdk.Context, proposal v1.Proposal) {
+	panic("unimplemented")
+}
+
+// UpdateMinDeposit implements GovKeeper.
+// Subtle: this method shadows the method (*Keeper).UpdateMinDeposit of Keeper.Keeper.
+func (keeper *Keeper) UpdateMinDeposit(ctx sdk.Context, checkElapsedTime bool) {
+	panic("unimplemented")
+}
+
+// UpdateMinInitialDeposit implements GovKeeper.
+// Subtle: this method shadows the method (*Keeper).UpdateMinInitialDeposit of Keeper.Keeper.
+func (keeper *Keeper) UpdateMinInitialDeposit(ctx sdk.Context, checkElapsedTime bool) {
+	panic("unimplemented")
 }
