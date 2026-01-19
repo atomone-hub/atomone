29ae5382f89d8f6b4d54de254bce755b419c2769
Julien Robert <julien@rbrt.fr>
2026-01-19 17:48:26 +0100

    updates

--- contrib/scripts/upgrade-node.sh
+++ contrib/scripts/upgrade-node.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+set -e
+
 # Usage ATOMONED_BIN=$(which simd) ATOMONED_NEW_BIN=<path to new binary> ./upgrade.sh
 
 if [[ -z $ATOMONED_BIN || -z $ATOMONED_NEW_BIN ]]; then
@@ -7,45 +9,50 @@ if [[ -z $ATOMONED_BIN || -z $ATOMONED_NEW_BIN ]]; then
     exit 1
 fi
 
+script_dir="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
+proposal_path="$script_dir/../localnet/proposal_upgrade.json"
 localnet_home=~/.atomone-localnet
+localnetd="$ATOMONED_BIN --home $localnet_home"
+localnetd_new="$ATOMONED_NEW_BIN --home $localnet_home"
 
 echo "init with previous atomoned binary"
-$ATOMONED_BIN init localnet --default-denom uatone --chain-id localnet
-$ATOMONED_BIN config set client chain-id localnet
-$ATOMONED_BIN config set client keyring-backend test
-$ATOMONED_BIN keys add val
-$ATOMONED_BIN genesis add-genesis-account val 1000000000000uatone,1000000000uphoton
-$ATOMONED_BIN keys add user
-$ATOMONED_BIN genesis add-genesis-account user 1000000000uatone,1000000000uphoton
-$ATOMONED_BIN genesis gentx val 1000000000uatone
-$ATOMONED_BIN genesis collect-gentxs
+rm -rf $localnet_home
+$localnetd init localnet --default-denom uatone --chain-id localnet
+$localnetd config chain-id localnet
+$localnetd config keyring-backend test
+$localnetd keys add val
+$localnetd genesis add-genesis-account val 1000000000000uatone,1000000000uphoton
+$localnetd keys add user
+$localnetd genesis add-genesis-account user 1000000000uatone,1000000000uphoton
+$localnetd genesis gentx val 1000000000uatone
+$localnetd genesis collect-gentxs
 # Add treasury DAO address
-$ATOMONED_BIN genesis add-genesis-account atone1qqqqqqqqqqqqqqqqqqqqqqqqqqqqp0dqtalx52 5388766663072uatone
+$localnetd genesis add-genesis-account atone1qqqqqqqqqqqqqqqqqqqqqqqqqqqqp0dqtalx52 5388766663072uatone
 # Add CP funds
-$ATOMONED_BIN genesis add-genesis-account atone1jv65s3grqf6v6jl3dp4t6c9t9rk99cd8flcml8 5388766663072uatone
-jq '.app_state.distribution.fee_pool.community_pool = [ { "denom": "uatone", "amount": "5388766663072.000000000000000000" }]' $(localnet_home)/config/genesis.json > /tmp/gen
-mv /tmp/gen $(localnet_home)/config/genesis.json
+$localnetd genesis add-genesis-account atone1jv65s3grqf6v6jl3dp4t6c9t9rk99cd8flcml8 5388766663072uatone
+jq '.app_state.distribution.fee_pool.community_pool = [ { "denom": "uatone", "amount": "5388766663072.000000000000000000" }]' $localnet_home/config/genesis.json > /tmp/gen
+mv /tmp/gen $localnet_home/config/genesis.json
 # Previous add-genesis-account call added the auth module account as a BaseAccount, we need to remove it
-jq 'del(.app_state.auth.accounts[] | select(.address == "atone1jv65s3grqf6v6jl3dp4t6c9t9rk99cd8flcml8"))' $(localnet_home)/config/genesis.json > /tmp/gen
-mv /tmp/gen $(localnet_home)/config/genesis.json
+jq 'del(.app_state.auth.accounts[] | select(.address == "atone1jv65s3grqf6v6jl3dp4t6c9t9rk99cd8flcml8"))' $localnet_home/config/genesis.json > /tmp/gen
+mv /tmp/gen $localnet_home/config/genesis.json
 # Set validator gas prices
-sed -i.bak 's#^minimum-gas-prices = .*#minimum-gas-prices = "0.01uatone,0.01uphoton"#g' $(localnet_home)/config/app.toml
+sed -i.bak 's#^minimum-gas-prices = .*#minimum-gas-prices = "0.01uatone,0.01uphoton"#g' $localnet_home/config/app.toml
 # enable REST API
-$ATOMONED_BIN config set app api.enable true
+sed -i -z 's/# Enable defines if the API server should be enabled.\nenable = false/enable = true/' $localnet_home/config/app.toml
 # Decrease voting period to 1min
-jq '.app_state.gov.params.voting_period = "60s"' $(localnet_home)/config/genesis.json > /tmp/gen
-mv /tmp/gen $(localnet_home)/config/genesis.json
-jq --rawfile data contrib/localnet/constitution-mock.md '.app_state.gov.constitution=$$data' $(localnet_home)/config/genesis.json > /tmp/gen
-mv /tmp/gen $(localnet_home)/config/genesis.json
+jq '.app_state.gov.params.voting_period = "60s"' $localnet_home/config/genesis.json > /tmp/gen
+mv /tmp/gen $localnet_home/config/genesis.json
+jq --rawfile data contrib/localnet/constitution-mock.md '.app_state.gov.constitution=$data' $localnet_home/config/genesis.json > /tmp/gen
+mv /tmp/gen $localnet_home/config/genesis.json
 echo "start chain"
-$ATOMONED_BIN start &
+$localnetd start &
 sleep 10
 echo "submit upgrade tx"
-$ATOMONED_BIN tx gov submit-proposal ../localnet/proposal_upgrade.json --from user --fees 10000uphoton --yes
+$localnetd tx gov submit-proposal "$proposal_path" --from user --fees 10000uphoton --yes
 sleep 6
-$ATOMONED_BIN tx gov deposit 1 10000000uatone --from user --fees 10000uphoton --yes
+$localnetd tx gov deposit 1 10000000uatone --from user --fees 10000uphoton --yes
 sleep 6
-$ATOMONED_BIN tx gov vote 1 yes --from user --yes
+$localnetd tx gov vote 1 yes --from val --yes
 echo "wait for chain halt and restart new binary"
-# $ATOMONED_NEW_BIN start --home $(localnet_home) &
-# $ATOMONED_NEW_BIN q tx gov params
+# $localnetd_new start &
+# $localnetd_new q tx gov params
