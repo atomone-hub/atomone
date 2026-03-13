0815ac92857687556aac2b0e74830798320849e6
Julien Robert <julien@rbrt.fr>
2026-03-13 13:15:52 +0100

    fix(x/dynamicfee): validate params on msg update

--- x/dynamicfee/types/msgs_test.go
+++ x/dynamicfee/types/msgs_test.go
@@ -5,6 +5,8 @@ import (
 
 	"github.com/stretchr/testify/require"
 
+	"cosmossdk.io/math"
+
 	sdk "github.com/cosmos/cosmos-sdk/types"
 
 	"github.com/atomone-hub/atomone/x/dynamicfee/types"
@@ -21,5 +23,21 @@ func TestMsgUpdateParams(t *testing.T) {
 		msg := types.NewMsgUpdateParams(sdk.AccAddress("test").String(), types.DefaultParams())
 		err := msg.ValidateBasic()
 		require.NoError(t, err)
+	})
+
+	t.Run("should reject a message with invalid params (Window=0)", func(t *testing.T) {
+		params := types.DefaultParams()
+		params.Window = 0
+		msg := types.NewMsgUpdateParams(sdk.AccAddress("test").String(), params)
+		err := msg.ValidateBasic()
+		require.Error(t, err)
+	})
+
+	t.Run("should reject a message with invalid params (TargetBlockUtilization=0)", func(t *testing.T) {
+		params := types.DefaultParams()
+		params.TargetBlockUtilization = math.LegacyZeroDec()
+		msg := types.NewMsgUpdateParams(sdk.AccAddress("test").String(), params)
+		err := msg.ValidateBasic()
+		require.Error(t, err)
 	})
 }
