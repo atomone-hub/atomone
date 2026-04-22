76fd80c45609c42bc1557f5bdda429fea6a361dc
Giuseppe Natale <12249307+giunatale@users.noreply.github.com>
2026-04-22 20:48:45 +0200

    chore: wire `x/epochs` (#323)
    Wire the `x/epochs` module, required for the nakamoto bonus added to
    `x/distribution`

--- app/keepers/keys.go
+++ app/keepers/keys.go
@@ -16,6 +16,7 @@ import (
 	banktypes "github.com/cosmos/cosmos-sdk/x/bank/types"
 	consensusparamtypes "github.com/cosmos/cosmos-sdk/x/consensus/types"
 	distrtypes "github.com/cosmos/cosmos-sdk/x/distribution/types"
+	epochstypes "github.com/cosmos/cosmos-sdk/x/epochs/types"
 	minttypes "github.com/cosmos/cosmos-sdk/x/mint/types"
 	paramstypes "github.com/cosmos/cosmos-sdk/x/params/types"
 	slashingtypes "github.com/cosmos/cosmos-sdk/x/slashing/types"
@@ -51,6 +52,7 @@ func (appKeepers *AppKeepers) GenerateKeys() {
 		photontypes.StoreKey,
 		dynamicfeetypes.StoreKey,
 		coredaostypes.StoreKey,
+		epochstypes.StoreKey,
 	)
 
 	// Define transient store keys
